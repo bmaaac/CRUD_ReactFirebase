e739fecb242367ded12883254edc0d72a83158db
bmaaac <bmac42969@gmail.com>
2024-10-30 07:04:06 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Navbar, Footer } from "./components/Layout";
 import Home from "./components/Home";
 import Write from "./components/Write";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
